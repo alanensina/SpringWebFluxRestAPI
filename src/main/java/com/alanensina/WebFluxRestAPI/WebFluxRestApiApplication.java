95e2e92bf5589b3c3231f5316b0092b06ee25495
alan.ensina <alan.ensina@stockinfo.com.br>
2019-05-03 14:12:07 -0300

    First commit!

---
+++ src/main/java/com/alanensina/WebFluxRestAPI/WebFluxRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.alanensina.WebFluxRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alanensina/WebFluxRestAPI/WebFluxRestApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alanensina.WebFluxRestAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebFluxRestApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
